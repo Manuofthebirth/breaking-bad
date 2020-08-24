8f97c2c304f987b9f680f36f888aff50a5be4219
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-24 18:57:08 -0300

    Link to added

--- src/components/pages/Saul.js
+++ src/components/pages/Saul.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import logo from '../../img/bblogo.png'
-import bcslogo from '../../img/bcs-logo.svg.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../img/bblogo.png';
+import bcslogo from '../../img/bcs-logo.svg.png';
 
 const Saul = () => {
   return (
     <header className='center'>
       <img className="logo" src={bcslogo} alt='better-call-saul-logo'/>
-      <img className="sm-logo" src={logo} alt='breaking-bad-logo'/>
+      <Link to="/"><img className="sm-logo" src={logo} alt='breaking-bad-logo'/></Link>
     </header>
   )
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import logo from '../../img/bblogo.png'
-import bcslogo from '../../img/bcs-logo.svg.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../img/bblogo.png';
+import bcslogo from '../../img/bcs-logo.svg.png';
 
 const Header = () => {
   return (
     <header className='center'>
       <img className="logo" src={logo} alt='breaking-bad-logo'/>
-      <img className="sm-logo" src={bcslogo} alt='better-call-saul-logo'/>
+      <Link to="/saul"><img className="sm-logo" src={bcslogo} alt='better-call-saul-logo'/></Link>
     </header>
   )
 }
